9b1906ebc9c36d53a46b17291ae1ad17d80e25a2
Joao Goncalves <joaopedro.jp.4998@gmail.com>
2021-02-14 00:47:29 +0000

    Fix articles responsiveness

--- assets/js/mouse.js
+++ assets/js/mouse.js
@@ -1,16 +1,19 @@
+var width = $(document).width(); 
+
+if ((width >= 550 )) {
   document.addEventListener("mousemove", logKey);
 
   function placeDiv(x_pos, y_pos) {
     var d = document.getElementById("1");
     var h = $(d).height();
-
+  
     d.style.position = "absolute";
     d.style.left = x_pos + "px";
     d.style.top = y_pos - h + "px";
-    console.log(y_pos);
   }
 
   function logKey(e) {
     placeDiv(e.screenX, e.screenY);
     placeDiv(e.clientX, e.clientY);
   }
+}
